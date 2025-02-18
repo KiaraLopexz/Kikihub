83de25827c422b37b79870728baeaa0d305bd0f1
KiaraLopexz <66958978+KiaraLopexz@users.noreply.github.com>
2025-02-17 22:56:59 -0300

    Update KiKiHub.lua

--- KiKiHub.lua
+++ KiKiHub.lua
@@ -66,4 +66,4 @@ closeButton.MouseButton1Click:Connect(function()
 end)
 
 minimizeButton.Position = UDim2.new(0, 0, 0, 0)
-closeButton.Position = UDim2.new(0, 25, 0, 0)
+closeButton.Position = UDim2.new(0, 0, 0, 0)
