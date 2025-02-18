78058b35a1bb6cd08c8ee7c9daab3a4d81a8b56d
KiaraLopexz <66958978+KiaraLopexz@users.noreply.github.com>
2025-02-17 22:38:44 -0300

    Update KiKiHub.lua

--- KiKiHub.lua
+++ KiKiHub.lua
@@ -23,50 +23,6 @@ innerBorder.Position = UDim2.new(0, 2, 0, 2)
 innerBorder.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 innerBorder.BorderSizePixel = 0
 
-local minimizeBtn = Instance.new("TextButton", frame)
-minimizeBtn.Size = UDim2.new(0, 30, 0, 30)
-minimizeBtn.Position = UDim2.new(1, -30, 0, 0)
-minimizeBtn.Text = "-"
-minimizeBtn.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-minimizeBtn.TextColor3 = Color3.fromRGB(255, 255, 255)
-minimizeBtn.BorderSizePixel = 0
-
-local maximizeBtn = Instance.new("TextButton", frame)
-maximizeBtn.Size = UDim2.new(0, 30, 0, 30)
-maximizeBtn.Position = UDim2.new(1, -60, 0, 0)
-maximizeBtn.Text = "⬜"
-maximizeBtn.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-maximizeBtn.TextColor3 = Color3.fromRGB(255, 255, 255)
-maximizeBtn.BorderSizePixel = 0
-
-local closeBtn = Instance.new("TextButton", frame)
-closeBtn.Size = UDim2.new(0, 30, 0, 30)
-closeBtn.Position = UDim2.new(1, -90, 0, 0)
-closeBtn.Text = "X"
-closeBtn.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-closeBtn.TextColor3 = Color3.fromRGB(255, 255, 255)
-closeBtn.BorderSizePixel = 0
-
-closeBtn.MouseButton1Click:Connect(function()
-    ui:Destroy()
-end)
-
-minimizeBtn.MouseButton1Click:Connect(function()
-    frame.Visible = not frame.Visible
-end)
-
-local isMaximized = false
-maximizeBtn.MouseButton1Click:Connect(function()
-    if isMaximized then
-        frame.Size = UDim2.new(0.4, 0, 0.4, 0)
-        maximizeBtn.Text = "⬜"
-    else
-        frame.Size = UDim2.new(0.8, 0, 0.8, 0)
-        maximizeBtn.Text = "↔"
-    end
-    isMaximized = not isMaximized
-end)
-
 frame.InputBegan:Connect(function(i) 
     if i.UserInputType == Enum.UserInputType.MouseButton1 then 
         dragging = true
