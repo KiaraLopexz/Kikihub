0fa9bde28237a0a196ce30a185eec1e01b093cb3
KiaraLopexz <66958978+KiaraLopexz@users.noreply.github.com>
2025-02-17 23:17:35 -0300

    Update KiKiHub.lua

--- KiKiHub.lua
+++ KiKiHub.lua
@@ -35,39 +35,4 @@ UIS.InputChanged:Connect(function(i)
     end 
 end)
 
-local buttonsFrame = Instance.new("Frame")
-buttonsFrame.Parent = frame
-buttonsFrame.BackgroundTransparency = 1
-buttonsFrame.Size = UDim2.new(0, 60, 0, 20)
-buttonsFrame.Position = UDim2.new(1, -60, 0, 0)
-
-local minimizeButton = Instance.new("TextButton")
-minimizeButton.Parent = buttonsFrame
-minimizeButton.Size = UDim2.new(0, 20, 0, 20)
-minimizeButton.Position = UDim2.new(0, 0, 0.5, -10)
-minimizeButton.AnchorPoint = Vector2.new(0.5, 0.5)
-minimizeButton.BackgroundTransparency = 1
-minimizeButton.Text = "_"
-minimizeButton.TextColor3 = Color3.fromRGB(255, 0, 0)
-minimizeButton.TextSize = 14
-minimizeButton.MouseButton1Click:Connect(function()
-    frame.Visible = false
-end)
-
-local closeButton = Instance.new("TextButton")
-closeButton.Parent = buttonsFrame
-closeButton.Size = UDim2.new(0, 20, 0, 20)
-closeButton.Position = UDim2.new(0, 30, 0.5, -10)
-closeButton.AnchorPoint = Vector2.new(0.5, 0.5)
-closeButton.BackgroundTransparency = 1
-closeButton.Text = "X"
-closeButton.TextColor3 = Color3.fromRGB(255, 0, 0)
-closeButton.TextSize = 14
-closeButton.MouseButton1Click:Connect(function()
-    ui:Destroy()
-end)
-
--- Movendo o texto do botão "X"
-closeButton.TextLabel.Position = UDim2.new(0.5, 0, 0.5, 0) -- Ajuste aqui a posição do texto
-minimizeButton.TextLabel.Position = UDim2.new(0.5, 0, 0.5, 0) -- Ajuste também o botão "-"
-
+local buttonsFrame = Inst
